89d90e91e45129393409a280eddc9d4c40f27fed
Busan <budi.santoso@alpabit.com>
2022-06-15 11:11:27 +0700

    import lombok

--- src/main/java/com/mini/project/tes/security/entity/JwtRefreshToken.java
+++ src/main/java/com/mini/project/tes/security/entity/JwtRefreshToken.java
@@ -1,9 +1,13 @@
 package com.mini.project.tes.security.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import javax.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.Date;
-
+@Data
+@AllArgsConstructor
 public class JwtRefreshToken {
     private String token;
     private String accessToken;
@@ -15,80 +19,4 @@ public class JwtRefreshToken {
     private String loginIp;
     private String workgroup;
 
-    public JwtRefreshToken() {
-    }
-
-    public JwtRefreshToken(String token) {
-        this.token = token;
-    }
-
-    public JwtRefreshToken(String token, SamUserEntity user, Instant expirationDateTime) {
-        this.token = token;
-        this.user = user;
-        this.expirationDateTime = expirationDateTime;
-    }
-    
-    public JwtRefreshToken(String token, String accessToken, SamUserEntity user, Instant expirationDateTime, Date createdDate) {
-        this.token = token;
-        this.accessToken = accessToken;
-        this.createdDate = createdDate;
-        this.user = user;
-        this.expirationDateTime = expirationDateTime;
-    }
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getAccessToken() {
-		return accessToken;
-	}
-
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
-
-
-    public String getLoginIp() {
-        return loginIp;
-    }
-
-    public void setLoginIp(String loginIp) {
-        this.loginIp = loginIp;
-    }
-
-    public String getWorkgroup() {
-        return workgroup;
-    }
-
-    public void setWorkgroup(String workgroup) {
-        this.workgroup = workgroup;
-    }
-
-    public Instant getExpirationDateTime() {
-        return expirationDateTime;
-    }
-
-    public void setExpirationDateTime(Instant expirationDateTime) {
-        this.expirationDateTime = expirationDateTime;
-    }
-
-	public Date getCreatedDate() {
-		return createdDate;
-	}
-
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
-
-    public SamUserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(SamUserEntity user) {
-        this.user = user;
-    }
 }
--- src/main/java/com/mini/project/tes/security/entity/LoginRequest.java
+++ src/main/java/com/mini/project/tes/security/entity/LoginRequest.java
@@ -1,30 +1,14 @@
 package com.mini.project.tes.security.entity;
 
-import javax.validation.constraints.NotBlank;
+import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+@Data
 public class LoginRequest {
     @NotBlank
     private String usernameOrEmail;
 
     @NotBlank
     private String password;
-
-
-
-    public String getUsernameOrEmail() {
-        return usernameOrEmail;
-    }
-
-    public void setUsernameOrEmail(String usernameOrEmail) {
-        this.usernameOrEmail = usernameOrEmail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
